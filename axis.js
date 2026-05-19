d0fa9fa944c32d890d62c4dce99c5725fd66412c
OnlyForShow <ZoomForDetail@gmx.de>
2026-05-19 19:45:26 +0200

    worked on parsing

--- axis.js
+++ axis.js
@@ -7,8 +7,10 @@ let width = 1000;
 let height = 500;
 let yend = ystart + height;
 
-let data = [[1,5,Math.exp(1)],[2,10,Math.exp(2)],[3,15,Math.exp(3)]];
+let data = [[1,5,Math.exp(1),"Wow"],[2,10,Math.exp(2),"Cool"],[3,15,Math.exp(3),"Epic"]];
 
+let axes = [];
+let tuple_data = []; 
 //If we want to rearrange the axes
 let order = [];
 
@@ -48,33 +50,72 @@ function initAxis()
     parseData();
 }
 
-let axes = [];
 
+
+
+//let table = [[1,5,Math.exp(1),"Wow"],[2,10,Math.exp(2),"Cool"],[3,15,Math.exp(3),"Epic"]];
+// Data[0] => [1,2,3]
+// Data[1] => [5,10,15]
+// Data[2] => [2.71,...]
+// Data[3] => ["Wow",
 function createInterpolation(data)
 {
+    if ( data.length === 0)
+    {
+        throw new Error("createInterpolation: Data must not be empty");
+    }
+    
+    let MIN_VALUE = Infinity;
+    let MAX_VALUE = -Infinity;
+        
     //Check whether data is numerical or categorical
-    if(Number.isFinite(data)
-    //If numerical then get the maximum and minimum data element
+    //isFinite() checks whether it is number
+    //Make the assumption that the first element type is representative of every element in the tuple
+    if(Number.isFinite(data[0]))
+    {
+        //If numerical then get the maximum and minimum data element
+        for(const elem in data)
+        {
+            if ( elem < MIN_VALUE ) MIN_VALUE = elem;
+            if ( elem > MAX_VALUE ) MAX_VALUE = elem;
+        }
 
-    //If categorial then put the data into set datastructure
-    //Get the total number of elements and save the order
-    //the set-elements by converting it to an array
-    //store this array or set in a seperate data structure
-}
+        return (x) => {
+            return (x - MIN_VALUE) / (MAX_VALUE - MIN_VALUE);
+        };
+        
+    }else
+    {
+    	//If categorial then put the data into map datastructure
+    	//Get the total number of elements and save the order
 
-class data_lookup_structure
-{
+        let ReferenceMap = new Map();
+        let index = 0;
+        for(const elem in data)
+        {
+            if(ReferenceMap.has(elem)) continue;
+            ReferenceMap.set(elem, index++);
+        }
+
+        MIN_VALUE = 0;
+        MAX_VALUE = ReferenceMap.size - 1;
+
+        return (x) => {
+            return (ReferenceMap.get(x)/MAX_VALUE);
+        };
+    }
 
 }
 
 function parseData()
 {
-   
-    for (let data_set of data)
+
+    for (let i = 0; i < data[0].length; i++)
     {
-        for( let i = 0 ; i < data_set.length; i++)
+        tuple_data.push([]);
+        for( let j = 0 ; j < data.length; j++)
         {
-            
+            tuple_data[i].push(data[j][i]);
         }
     }
 
@@ -88,7 +129,7 @@ function parseData()
         tmp.name = i + ".";
         
         //Set the interpolation function which calculates max and min of the data set
-        tmp.interpolation = createInterpolation(data);
+        tmp.interpolation = createInterpolation(tuple_data[i]);
         tmp.color = "#000000";
         axes.push(tmp);
         
@@ -117,7 +158,7 @@ function drawAxes(ctx)
         let current_axis = axes[axis_pos_1];
         let next_axis = axes[axis_pos_2];
 
-        //draw first axis
+        //draw axis
         ctx.beginPath();
         ctx.moveTo(x_pos, ystart);
         ctx.lineTo(x_pos, yend);
@@ -128,6 +169,17 @@ function drawAxes(ctx)
         {
             
         	//draw connections
+
+            for(const data_tuple in data)
+            {
+                const value_current_axis = data_tuple[axis_pos_1];
+                const value_next_axis = data_tuple[axis_pos_2];
+
+                current_axis_x_pos = current_axis.interpolation(value_current_axis);
+            }
+             
+            current_axis.interpolation(
+            
         	//ctx.beginPath();
         	//ctx.moveTo(x_pos + distance, ystart);
         	//ctx.lineTo(x_pos + distance, yend);
