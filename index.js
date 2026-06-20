53684063783abdedb2fd5657676a6c391d3eeb8a
OnlyForShow <ZoomForDetail@gmx.de>
2026-06-20 18:50:16 +0200

    fixed scaling issue

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ function init() {
 
 
 
-
+g
     
-<<<<<<< HEAD
+
     parallelCoordinates_visual = new ParallelCoordinates(0,0,window.innerWidth * window.devicePixelRatio,
                                                          window.innerHeight * window.devicePixelRatio);
-=======
-    parallelCoordinates_visual = new ParallelCoordinates(0,0,window.innerWidth* window.devicePixelRatio,window.innerHeight* window.devicePixelRatio);
->>>>>>> 9f6ac9da3f97466c5a4faab115882be050a84234
+
+
+
     parallelCoordinates_visual.parseData(example_data);
     
     
